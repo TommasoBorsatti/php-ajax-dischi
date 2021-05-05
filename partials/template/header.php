b02748b8583a60c05971eb79c3eda1c1ad418a66
Tommaso Borsatti <tommasoborsatti89@gmail.com>
2021-05-05 16:39:14 +0200

    Prima metodo senza VUE  JS - non funziona il comando DIR!
    Ho riprodotto l'esercizio usando piu file php con includes; le istruzioni v-for di vue sono state sostituite da un for each che prende i dati  da un database php sottoforma di un array. 
    
    Purtroppo non funziona la costante DIR e devo inserire i path relativi basandomi sempre sull index.
    
    Alcuni problemi con lo stile css, da rivedere

---
+++ partials/template/header.php
@@ -0,0 +1,12 @@
+
+<header>
+  <div class="logo">
+    <img src="img/logo.png" alt="logo di Spotify">
+  </div>
+  <div class="g-selection">
+    <label for="genres">Scegli il tuo genere di musica preferito:</label>
+    <select id="genres" name="genres">
+      <option value="All">All</option>
+    </select>
+  </div>
+</header>
